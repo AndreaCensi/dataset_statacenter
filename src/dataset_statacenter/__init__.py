d714e4010d4af155bf563dbee81f8115b1b61e24
Andrea Censi <andrea@cds.caltech.edu>
2014-08-04 16:13:31 -0700

    minor

--- src/dataset_statacenter/__init__.py
+++ src/dataset_statacenter/__init__.py
@@ -1,20 +1,13 @@
 
-
-
-
 def jobs_comptests(context):
-    from comptests import jobs_registrar
+    from conf_tools import GlobalConfig
+    GlobalConfig.global_load_dirs(['dataset_statacenter.configs'])
 
-    # get testing configuration directory 
-    from pkg_resources import resource_filename  # @UnresolvedImport
-    dirname = resource_filename("dataset_statacenter", "configs")
-    
-    # load into rawlogs config
-    from rawlogs import get_rawlogs_config
-    config = get_rawlogs_config()
-    config.load(dirname)
-    
     # Our tests are its tests with our configuration
     from rawlogs import unittests
-    j1 = jobs_registrar(context, config)
-    return j1
+
+    from comptests import jobs_registrar
+    from rawlogs import get_rawlogs_config
+    jobs_registrar(context, get_rawlogs_config())
+
+
